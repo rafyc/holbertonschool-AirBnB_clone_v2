5b12b67eba9ac6095e5d00ff95ab78814db46033
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-25 12:11:59 +0200

    fix sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,5 +9,5 @@ fi
 mkdir -p /data/web_static/{releases/test,shared} && echo "<html><head></head><body><h1>hello world</h1></body></html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/:80 default_server;/a location /hbnb_static/ {alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a location /hbnb_static/ {alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
