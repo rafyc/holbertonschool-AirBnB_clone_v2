0b5c42e89aee1e9e5011acef96cb60e07561acee
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-25 09:36:03 +0200

    add html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,20 @@ if [ ! -x /usr/sbin/nginx ]; then
     apt-get -y install nginx
 fi
 
-mkdir -p /data/web_static/{releases/test,shared} && echo "hello world" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/{releases/test,shared} && echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/:80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
