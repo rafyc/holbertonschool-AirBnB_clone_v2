930fe610d36e7938a74916812c35b2300ca44624
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-08-25 10:03:11 +0200

    fix pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,46 +3,29 @@
 archive from the contents of the web_static folder
 of your AirBnB Clone repo, using the function do_pack."""
 
-from fabric.api import local, put, env, run
+from fabric.api import local, put, run, env
 from datetime import datetime
 from os.path import exists
-
 env.hosts = ['54.209.251.89', '54.147.180.101']
 
-def do_pack():
-    """
-    function that create an archiv .tgz
-    """
-    time = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
-    local('mkdir -p versions')
-    file_name = 'versions/web_static_{}.tgz'.format(time)
-    try:
-        local('tar -czvf {} web_static'.format(file_name))
-        return file_name
-    except Exception:
-        return None
 
 def do_deploy(archive_path):
-    """ The function do_deploy """
-    if not exists(archive_path):
+    """ deploy archive on server """
+    if exists(archive_path) is False:
         return False
     try:
-        """archive_path = versions/web_static_20170315003959.tgz """
-        file_name = archive_path.split("/")[-1]
-        """file_name = web_static_20170315003959.tgz"""
-        file_with_no_ext = file_name.split(".")[0]
-        """file_with_no_ext = web_static_20170315003959"""
+        filename = archive_path.split('/')[-1]
+        fname_noext = filename.split('.')[0]
         path = "/data/web_static/releases/"
-        """Upload the archive to the /tmp/ directory of the web server"""
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_with_no_ext))
-        """Uncompress the archive"""
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_with_no_ext))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_with_no_ext))
-        run('rm -rf {}{}/web_static'.format(path, file_with_no_ext))
+        run('mkdir -p {}{}/'.format(path, fname_noext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, path, fname_noext))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, fname_noext))
+        run('rm -rf {}{}/web_static'.format(path, fname_noext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_with_no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, fname_noext))
+        print("New version deployed!")
         return True
     except Exception:
         return False
