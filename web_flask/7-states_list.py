38294490290439bc2c0d76cd642f798b7477d80d
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-08 15:37:23 +0200

    7 html and py file

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from models import storage
+from models import state
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_db():
+    storage.close()
+
+@app.route("/states_list", strict_slashes=False)
+def state():
+    list_state = storage.all(state).values
+    print(list_state)
+    return render_template('7-states_list.html', list=list_state)
