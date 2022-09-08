52ee82f0c961a2cc8c390197440f645a732aaa9a
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-08 15:47:35 +0200

    7 html and py file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 from flask import Flask, render_template
 from models import storage
-from models import state
+from models.state import State
 
 app = Flask(__name__)
 
