82cb6900fd93a01e4f90cad8ef0fcdc38414432d
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-08 10:33:47 +0200

    first push

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/", strict_slashes = False)
+
+@app.route("/", strict_slashes=False)
 def hello_world():
     return "<p>Hello HBNB!</p>"
 
+
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
