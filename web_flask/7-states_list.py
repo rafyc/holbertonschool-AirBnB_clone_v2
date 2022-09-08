22612501ee8c93c2c208c9e5839c2caa973c32f3
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-08 15:39:37 +0200

    7 html and py file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,3 +13,7 @@ def state():
     list_state = storage.all(state).values
     print(list_state)
     return render_template('7-states_list.html', list=list_state)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
