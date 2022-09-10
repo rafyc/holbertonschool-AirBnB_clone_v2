2cbb86c95194a425016378a3fd38393e22280398
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-10 09:32:22 +0200

    fix fstring

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-''' update some part of our engine:'''
+"""This module starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def state():
-    '''
-    '''
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """The method lists all State"""
     list_state = storage.all(State).values()
-    return render_template('7-states_list.html', list=list_state)
+    return render_template('7-states_list.html', list_state=list_state)
 
 
 @app.teardown_appcontext
-def teardown_db(execption):
-    '''
-    '''
+def tear_down(exception):
+    """The method remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
