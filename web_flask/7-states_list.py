2e9bea639c2f94dab764bc8fd316dbad9992fb62
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-10 09:31:34 +0200

    fix fstring

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,19 +7,19 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(execption):
-    '''
-    '''
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def state():
     '''
     '''
     list_state = storage.all(State).values()
     return render_template('7-states_list.html', list=list_state)
+
+
+@app.teardown_appcontext
+def teardown_db(execption):
+    '''
+    '''
+    storage.close()
 
 
 if __name__ == '__main__':
