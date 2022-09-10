52eed47127df64435dd3335fb6a80466ccac9469
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-10 09:06:11 +0200

    fix comment

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Starts a Flask web app listening on 0.0.0.0 port 5000"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_world():
-    '''
+    '''method displays
     '''
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    '''
+    '''method displays
     '''
     return "HBNB"
 
