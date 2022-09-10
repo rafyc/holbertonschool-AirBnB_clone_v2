5d726806253dce6bdf68048fd4d1b364915a7038
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-10 10:55:53 +0200

    fix pycode

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
-from flask import Flask, render_template
+''' script that starts a Flask web application '''
+
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
+def index():
+    ''' Function that display Hello HBNB! '''
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Prints a Message when /hbnb is called """
+    ''' Function that display HBNB '''
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ Prints a Message when /c is called """
-    return "C " + text.replace('_', ' ')
+def variable(text):
+    ''' Function that display C + <text> '''
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is_cool'):
-    """ Prints a Message when /python is called """
-    return "Python " + text.replace('_', ' ')
+def python(text='is cool'):
+    ''' Function that display Python + <text> '''
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_n_number(n):
-    """ Prints a Message when /number is called only if n is an int"""
-    return "{:d} is a number".format(n)
+def number(n):
+    ''' Function that display <n> '''
+    return str(n) + ' is a number'
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """ display a HTML page only if n is an integer """
-    return render_template('5-number.html', value=n)
+def template(n):
+    ''' Function that display template '''
+    return render_template('5-number.html', num=n)
 
 
-if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
